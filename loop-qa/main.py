2539d14378a521b67a986471e00c1087b6d27197
Nick Heiner <nick.heiner+github@gmail.com>
2023-05-01 14:10:16 -0400

    Use a better URL pattern

--- loop-qa/main.py
+++ loop-qa/main.py
@@ -8,20 +8,12 @@ Fixie agent example:
 """
 
 import fixieai
-import requests
-
-base_url = "https://ridewithloop.zendesk.com/api/v2/help_center/en-us/articles.json?page=1"
-
-response = requests.get(base_url)
-json_data = response.json()
-
-highest_page = json_data["page_count"]
-valid_urls = [f"{base_url}{page}" for page in range(1, highest_page + 1)]
 
 BASE_PROMPT = """I am an agent that helps developers write Fixie agents.
 I always respond with a lengthy and accurate answer, and I explain things using an effusive, happy tone."""
 
-CORPORA = [fixieai.DocumentCorpus(urls=valid_urls)]
+URLS = ["https://help.ridewithloop.com/*"]
+CORPORA = [fixieai.DocumentCorpus(urls=URLS)]
 agent = fixieai.CodeShotAgent(
     BASE_PROMPT,
     [],
