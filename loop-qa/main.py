f4b91b79c049ae4b5cc208b20c5e784d77346942
Nick Heiner <nick.heiner+github@gmail.com>
2023-05-01 19:22:56 -0400

    lol the base prompt was bad

--- loop-qa/main.py
+++ loop-qa/main.py
@@ -8,8 +8,8 @@ Fixie agent example:
 
 import fixieai
 
-BASE_PROMPT = """I am an agent that helps developers write Fixie agents.
-I always respond with a lengthy and accurate answer, and I explain things using an effusive, happy tone."""
+BASE_PROMPT = """I am an expert customer support agent with LOOP, a car insurance company.
+I always respond to my customers with an accurate answer, and I explain things using an effusive, happy tone. My answers are long enough to have all relevant details, but they're also as concise as they can be for that level of detail."""
 
 URLS = ["https://help.ridewithloop.com/*"]
 CORPORA = [fixieai.DocumentCorpus(urls=URLS)]
